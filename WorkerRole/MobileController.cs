88ce6416c7380e7a6f7766ebd54d17e12e0f0e30
Thai Dang <dangtranthaidtv50dh@gmail.com>
2023-04-19 20:08:05 +0700

    azure hÃosting

---
+++ WorkerRole/MobileController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WorkerRole.Model;
+
+namespace WorkerRole
+{
+    public class MobileController : ApiController
+    {
+        private List<Mobile> _products = new List<Mobile>
+        {
+            new Mobile {Id = 1, Name = "Samsung"},
+            new Mobile {Id = 2, Name = "Iphone"},
+            new Mobile {Id = 3, Name = "Nokia"},
+            new Mobile {Id = 4, Name = "Huwei"},
+            new Mobile {Id = 5, Name = "S:"}
+        };
+
+        //api//products
+        public IEnumerable<Mobile> Get()
+        {
+            return _products;
+        }
+
+        // api/products/id
+        public Mobile Get(int id)
+        {
+            var product = _products.FirstOrDefault(c => c.Id == id);
+            if (product == null)
+            {
+                throw new HttpResponseException(
+                    System.Net.HttpStatusCode.NotFound);
+            }
+            return product;
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Post(Mobile product)
+        {
+            try
+            {
+                _products.Add(product);
+                return Request.CreateResponse(HttpStatusCode.OK, _products);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+           
+        }
+    }
+}
---
+++ WorkerRole/WorkerRole.cs
@@ -0,0 +1,62 @@
+using Microsoft.Owin.Hosting;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkerRole
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
+        private IDisposable _app = null;
+        public override void Run()
+        {
+            Trace.TraceInformation("WorkerRole is running");
+
+            while (true)
+            {
+                Thread.Sleep(10000);
+                Trace.TraceInformation("Working");
+            }
+        }
+
+        public override bool OnStart()
+        {
+            ServicePointManager.DefaultConnectionLimit = 12;
+            var testEnpoint = RoleEnvironment.CurrentRoleInstance.InstanceEndpoints["MobileEndpoint"];
+            string baseUri = string.Format("{0}://{1}/mobile", testEnpoint.Protocol, testEnpoint.IPEndpoint);
+            _app = WebApp.Start<Startup>(new StartOptions(url: baseUri));
+            return base.OnStart();
+        }
+
+        public override void OnStop()
+        {
+            Trace.TraceInformation("WorkerRole is stopping");
+
+            this.cancellationTokenSource.Cancel();
+            this.runCompleteEvent.WaitOne();
+
+            base.OnStop();
+
+            Trace.TraceInformation("WorkerRole has stopped");
+        }
+
+        private async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Replace the following with your own logic.
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Trace.TraceInformation("Working");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
